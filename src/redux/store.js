f84002d313b37e32d83cee568ae7f48c3302acc4
Manikanta-Pothuboina <manichinna1998@gmail.com>
2021-07-17 16:03:12 +0530

    TaskAssign

---
+++ src/redux/store.js
@@ -0,0 +1,34 @@
+import {
+  applyMiddleware,
+  combineReducers,
+  createStore,
+} from "@reduxjs/toolkit";
+import thunk from "redux-thunk";
+import { AdminReducer } from "./AdminReducer";
+import { EmployeeReducer } from "./EmployeeReducer";
+import { EmployeeTaskReducer } from "./EmployeeTaskReducer";
+import { AdminTaskTimeExtensionRequestsReducer } from "./AdminTaskTimeExtensionRequestsReducer";
+import { EmployeeRequestReducer } from "./EmployeeRequestReducer";
+import { UserReducer } from "./UserReducer";
+import { BenchReducer } from "./BenchReducer";
+import { TaskAssignReducer } from "./TaskAssignReducer";
+
+const rootReducer = combineReducers({
+  admin: AdminReducer,
+  user: UserReducer,
+  employee: EmployeeReducer,
+  employeeTask: EmployeeTaskReducer,
+  adminTaskTimeExtensionRequest: AdminTaskTimeExtensionRequestsReducer,
+  employeeRequest: EmployeeRequestReducer,
+  bench: BenchReducer,
+  assign: TaskAssignReducer,
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+export { store };
+
+// HOW TO ACCESS THE STORE IN COMPOENNT
+// state.employeeList :: when there was only one reducer.
+// state.authSuccess :: when there was only one reducer.
+// state.employee.employeeList
+// state.user.authSuccess
